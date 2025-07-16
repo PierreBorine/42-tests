0f38a2b3ad83ddab5dff8ba3c7fe4340ca5fa02e
Pierre Fauveaux <pfauveau@fu-r3-p2.s19.be>
2025-07-16 15:35:52 +0200

    feat: script to install some tools

--- install.sh
+++ install.sh
@@ -8,24 +8,30 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[39m'
 
-SCRIPT_PATH=${BASH_SOURCE%/*}
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 gen_header() {
 	$SCRIPT_PATH/.bin/header-gen $@
 }
 
-failure() {
-	printf "${RED}"
-	gen_header "FAILURE"
-	echo $1
-	printf "${NC}"
-	exit 1
-}
+mkdir -p /tmp/tests_c_pf/bin
 
-error() {
-	printf "${RED}"
-	gen_header "ERROR"
-	echo $1
-	printf "${NC}"
-	exit 1
-}
+printf "${GREEN}"
+gen_header "INSTALLING BAT"
+printf "${NC}"
+
+bat_version="0.25.0"
+if [ ! -f /tmp/tests_c_pf/bin/bat_${bat_version}_amd64.deb ]; then
+	curl -L \
+		https://github.com/sharkdp/bat/releases/download/v${bat_version}/bat_${bat_version}_amd64.deb \
+		-o /tmp/tests_c_pf/bin/bat_${bat_version}_amd64.deb
+fi
+dpkg --extract /tmp/tests_c_pf/bin/bat_${bat_version}_amd64.deb \
+	/tmp/tests_c_pf/bin/
+mv /tmp/tests_c_pf/bin/usr/bin/bat $SCRIPT_PATH/.bin/bat
+
+echo "fish: 'alias bat $SCRIPT_PATH/.bin/bat'"
+echo "fish: 'alias cat \"$SCRIPT_PATH/.bin/bat -pp\"'"
+echo
+echo "posix: 'alias bat=$SCRIPT_PATH/.bin/bat'"
+echo "posix: 'alias cat=\"$SCRIPT_PATH/.bin/bat -pp\"'"
