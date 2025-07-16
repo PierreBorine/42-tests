b62230cb6c463380ef8aeef9fb8716fa5473827e
Pierre Fauveaux <pfauveau@fu-r3-p2.s19.be>
2025-07-16 12:11:21 +0200

    feat(C01): better ex00 message

--- C01/ex00.c
+++ C01/ex00.c
@@ -4,8 +4,9 @@ void	ft_ft(int *nbr);
 
 int	main(void)
 {
-	int	test_int;
+	int	test_int = 19;
 
+	printf("Initial: %d\n", test_int);
 	ft_ft(&test_int);
-	printf("%d", test_int);
+	printf("Final: %d\n", test_int);
 }
