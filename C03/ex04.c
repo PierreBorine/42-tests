af05b997670ccb9c1cc5c5c3d0b98f6bd7352a5c
Pierre Fauveaux <pfauveau@fu-r3-p2.s19.be>
2025-07-16 15:36:32 +0200

    refactor(C03): slightly better ex04 test message

--- C03/ex04.c
+++ C03/ex04.c
@@ -4,32 +4,23 @@
 char	*ft_strstr(char *str, char *to_find);
 
 #define MY_TEST(stack, needle) \
-	printf("(\"%s\") => \"%s\"\n", needle, ft_strstr(stack, needle))
+	printf("(\"%s\") => %s\n", needle, ft_strstr(my_stack, needle))
 
 #define THE_TEST(stack, needle) \
-	printf("(\"%s\") => \"%s\"\n", needle, strstr(stack, needle))
+	printf("(\"%s\") => %s\n", needle, strstr(my_stack, needle))
 
 int	main(void)
 {
 	char my_stack[] = "Hello World !";
-	printf("TESTING: (\"%s\")\n", my_stack);
+	printf("STACK: \"%s\"", my_stack);
+
 	printf("MINE:\n----------\n");
 	MY_TEST(my_stack, "rl");
 	MY_TEST(my_stack, "rrl");
 	MY_TEST(my_stack, "");
-	printf("REFERENCE:\n----------\n");
+
+	printf("\nREFERENCE:\n----------\n");
 	THE_TEST(my_stack, "rl");
 	THE_TEST(my_stack, "rrl");
 	THE_TEST(my_stack, "");
-
-	char my_stack2[] = "";
-	printf("\nTESTING: (\"%s\")\n", my_stack2);
-	printf("MINE:\n----------\n");
-	MY_TEST(my_stack2, "rl");
-	MY_TEST(my_stack2, "rrl");
-	MY_TEST(my_stack2, "");
-	printf("REFERENCE:\n----------\n");
-	THE_TEST(my_stack2, "rl");
-	THE_TEST(my_stack2, "rrl");
-	THE_TEST(my_stack2, "");
 }
