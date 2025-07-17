fba85b70482f3c726bcf537dc9ce52748afdaad4
Pierre Fauveaux <pfauveau@fu-r3-p2.s19.be>
2025-07-17 16:37:36 +0200

    some things

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[39m'
 
-SCRIPT_PATH=${BASH_SOURCE%/*}
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 gen_header() {
 	$SCRIPT_PATH/.bin/header-gen $@
@@ -22,6 +22,13 @@ failure() {
 	exit 1
 }
 
+soft_error() {
+	printf "${RED}"
+	gen_header "ERROR"
+	echo $1
+	printf "${NC}"
+}
+
 error() {
 	printf "${RED}"
 	gen_header "ERROR"
@@ -31,7 +38,7 @@ error() {
 }
 
 if [[ -z "$1" ]]; then
-	failure "You need to give at least a project name to test. (ex: C02)"
+	error "You need to give at least a project name to test. (ex: C02)"
 fi
 
 project=$1
@@ -48,39 +55,60 @@ printf "${NC}"
 
 
 if ! [[ "$project" =~ ^C[0-9]{2}$ ]]; then
-	failure "Argument '$project' does not match any known project."
+	error "Argument '$project' does not match any known project."
 fi
 if ! [[ -d "$SCRIPT_PATH/$project" ]]; then
-	failure "No tests for '$project'."
+	error "No tests for '$project'."
 fi
 if [[ -z "$( ls -A $SCRIPT_PATH/$project )" ]]; then
-	failure "No tests for '$project'."
+	error "No tests for '$project'."
 fi
 
 
 run_exercie() {
+	if ! [ -f "$SCRIPT_PATH/$project/$1.c" -o -f "$SCRIPT_PATH/$project/$1.sh" ]; then
+		soft_error "No tests for '$project/$1'."
+		return
+	fi
+
 	mkdir -p /tmp/tests_c_pf
 	echo
 	printf "${CYAN}"
 	gen_header "RUNNING: $1"
 	printf "${NC}"
 	echo
+
+	# Run the norminette
+	# if ! $(norminette ./$project/$1 -R CheckForbiddenSourceHeader); then
+	# 	failure "Norminette failed !"
+	# fi
+
+	test_file=""
+	if [ -f "$SCRIPT_PATH/$project/$1.c" ]; then
+		test_file="$SCRIPT_PATH/$project/$1.c"
+	fi
+
 	# compile with flags
 	cc -lbsd -Wextra -Wall -Werror \
-		"$SCRIPT_PATH/$project/$1.c" \
+		$test_file \
 		./$project/$1/*.c \
 		-o /tmp/tests_c_pf/$1.o
-	# execute the test program
-	/tmp/tests_c_pf/$1.o
+
+	if [ -f "$SCRIPT_PATH/$project/$1.c" ]; then
+		/tmp/tests_c_pf/$1.o
+	elif [ -f "$SCRIPT_PATH/$project/$1.sh" ]; then
+		"$SCRIPT_PATH/$project/$1.sh" "/tmp/tests_c_pf/$1.o"
+	fi
+	
+	# else
+	# 	failure "Compilation failed !"
+	# fi
 }
 
 run_all() {
-	for ex_dir in ./$project/*/
-	do
+	for ex_dir in ./$project/*/; do
 		ex_dir=$(basename $ex_dir)
-		if [ -f "$SCRIPT_PATH/$project/$ex_dir.c" ]; then
-			run_exercie $ex_dir
-		fi
+		run_exercie $ex_dir
 	done
 }
 
@@ -96,11 +124,7 @@ if [ -z "$2" ]; then
 else
 	exercice=$2
 	if ! [[ "$exercice" =~ ^ex[0-9]{2}$ ]]; then
-		failure "Argument '$exercice' does not match any known exercice."
+		error "Argument '$exercice' does not match any known exercice."
 	fi
-	if [ ! -f "$SCRIPT_PATH/$project/$exercice.c" ]; then
-		failure "No tests for '$project/$exercice'."
-	fi
-
 	run_exercie $exercice
 fi
